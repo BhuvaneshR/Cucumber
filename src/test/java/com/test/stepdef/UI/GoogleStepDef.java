5c6abeb22fb2ecc1aeeb4b0976c818b3ac70433e
bhuvaneshR <bhuvanesh.ravilla@zohomail.com>
2024-03-18 22:40:16 +0530

    Converted Page class to Interface

--- src/test/java/com/test/stepdef/UI/GoogleStepDef.java
+++ src/test/java/com/test/stepdef/UI/GoogleStepDef.java
@@ -1,16 +1,17 @@
 package com.test.stepdef.UI;
 
+import com.test.pages.IFastPage;
 import com.test.utilities.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 
-import com.test.pages.GooglePage;
+import com.test.pages.implimentations.FastPageImpl;
 import com.test.utilities.BasePageObject;
 
 import io.cucumber.java.en.*;
 
 public class GoogleStepDef extends BasePageObject {
 
-	GooglePage gpage;
+	IFastPage gpage;
 	WebDriver driver;
 
 	@Given("user launches {string}")
@@ -22,17 +23,17 @@ public class GoogleStepDef extends BasePageObject {
 		}
 		driver.get(URL);
 		driver.manage().window().maximize();
-		gpage=new GooglePage(driver);
+		gpage=new FastPageImpl(driver);
 	}
 
 	@When("user enters his name {string}")
 	public void user_enters_his_name(String Name) {
-		gpage.sendKeystoSearchBar(Name);
+		//gpage.sendKeystoSearchBar(Name);
 	}
 
 	@When("clicks on search button")
 	public void clicks_on_search_button() {
-		gpage.clickSearchBtn();
+		//gpage.clickSearchBtn();
 	}
 
 	@Then("this linked profile is visible")
